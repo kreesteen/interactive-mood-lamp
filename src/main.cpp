f776a1d7ab98c305494b150c13852b8b44f91574
kreesteen <kcbitoon05@gmail.com>
2026-05-29 18:28:26 +1000

    added button functionality for rotary encoder sw. there are 2 versions for the main loop - the second one is better for timing (doesn't use delay()) and has cleaner code

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,22 @@ const int BLUE_PIN = 47;
 
 const int OUTPUT_A = 40;
 const int OUTPUT_B = 41;
+const int OUTPUT_BUTTON = 42;
 
 int brightness = 0;
+int lastBrightness = -1;
 
 int lastEncoderValue = 0;
 
+int lastButtonReading = HIGH;
+int buttonStableState = HIGH;
+
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 50;
+
+unsigned long previousMillis = 0;
+
+bool debounceButton();
 void setColour(int red, int green, int blue);
 int rotaryEncoder();
 
@@ -21,36 +32,79 @@ void setup() {
   pinMode(BLUE_PIN, OUTPUT);
   pinMode(OUTPUT_A, INPUT_PULLUP);
   pinMode(OUTPUT_B, INPUT_PULLUP);
+  pinMode(OUTPUT_BUTTON, INPUT_PULLUP);
 }
 
+// void loop() {
+//   // for (int i = 0; i < 255; i += 10) {
+//   //   analogWrite(RED_PIN, i);
+//   //   delay(30);
+//   // }
+
+//   // for (int j = 255; j > 0; j -= 10) {
+//   //   analogWrite(RED_PIN, j);
+//   //   delay(30);
+//   // }
+
+//   int encoderDelta = rotaryEncoder();
+//   brightness += encoderDelta * 10;
+//   if (brightness < 0) {
+//     brightness = 0;
+//   }
+//   else if (brightness > 255) {
+//     brightness = 255;
+//   }
+
+
+//   analogWrite(RED_PIN, brightness);
+
+//   // if (brightness != lastBrightness) {
+//   //   Serial.print("Brightness: ");
+//   //   Serial.println(brightness);
+//   //   lastBrightness = brightness;
+//   // }
+
+//   // delay(1);
+
+//   currentMillis = millis();
+//   if (currentMillis - previousMillis >= 10) {
+//     previousMillis = currentMillis;
+    
+//     if (debounceButton()) {
+//     Serial.println("Button pressed!");
+//     }
+//   }
+
+  
+
+//   // if (encoderDelta != lastEncoderValue) {
+//   //   Serial.println(encoderDelta);
+//   //   lastEncoderValue = encoderDelta;
+//   // }
+
+// }
+
+
 void loop() {
-  // for (int i = 0; i < 255; i += 10) {
-  //   analogWrite(RED_PIN, i);
-  //   delay(30);
-  // }
 
-  // for (int j = 255; j > 0; j -= 10) {
-  //   analogWrite(RED_PIN, j);
-  //   delay(30);
-  // }
+    unsigned long currentMillis = millis();
 
-  int encoderDelta = rotaryEncoder();
-  brightness += encoderDelta * 10;
-  if (brightness < 0) {
-    brightness = 0;
-  }
-  else if (brightness > 255) {
-    brightness = 255;
-  }
+    if (currentMillis - previousMillis >= 2) {
 
+        previousMillis = currentMillis;
 
-  analogWrite(RED_PIN, brightness);
-  Serial.println(brightness);
-  // if (encoderDelta != lastEncoderValue) {
-  //   Serial.println(encoderDelta);
-  //   lastEncoderValue = encoderDelta;
-  // }
+        int encoderDelta = rotaryEncoder();
 
+        brightness += encoderDelta * 10;
+
+        brightness = constrain(brightness, 0, 255);
+
+        if (debounceButton()) {
+            Serial.println("Button pressed!");
+        }
+
+        analogWrite(RED_PIN, brightness);
+    }
 }
 
 void setColour(int red, int green, int blue) {
@@ -93,4 +147,33 @@ int rotaryEncoder() {
 
   return val;
 
+}
+
+bool debounceButton() {
+
+  int reading = digitalRead(OUTPUT_BUTTON);
+
+  // reset timer if input changed
+  if (reading != lastButtonReading) {
+    lastDebounceTime = millis();
+  }
+
+  // check if stable long enough
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+
+    // accept new stable state
+    if (reading != buttonStableState) {
+
+      buttonStableState = reading;
+
+      // detect actual press
+      if (buttonStableState == LOW) {
+        lastButtonReading = reading;
+        return true;
+      }
+    }
+  }
+
+  lastButtonReading = reading;
+  return false;
 }
